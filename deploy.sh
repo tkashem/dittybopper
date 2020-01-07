10f05f293e29de3043a0f96b113142703e6fed14
Joe Talerico <jtaleric@redhat.com>
2020-01-07 15:51:41 -0500

    Adding default performance dashboard

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 function _usage {
   cat <<END
-  
+
 Deploys a mutable grafana pod with default dashboards for monitoring
 system submetrics during workload/benchmark runs
 
@@ -44,7 +44,7 @@ grafana_default_pass=True
 
 # Other vars
 deploy_template="templates/dittybopper.yaml.template"
-dashboards=(https://raw.githubusercontent.com/cloud-bulldozer/arsenal/master/system-metrics-dashboards/grafana/master_nodes.json https://raw.githubusercontent.com/cloud-bulldozer/arsenal/master/kube-cluster-dashboard/grafana/kube_cluster.json)
+dashboards=(https://raw.githubusercontent.com/cloud-bulldozer/arsenal/master/openshift-performance-dashboard/grafana/on-cluster-latest.json https://raw.githubusercontent.com/cloud-bulldozer/arsenal/master/system-metrics-dashboards/grafana/master_nodes.json https://raw.githubusercontent.com/cloud-bulldozer/arsenal/master/kube-cluster-dashboard/grafana/kube_cluster.json)
 
 # Capture and act on command options
 while getopts ":c:m:n:p:i:dh" opt; do
